afa1ef9316b4c96002db62750747703816002b45
shimmingyu <alsrb60@petpuls.net>
2025-07-25 11:58:14 +0900

    test commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import './style.css'
-import FreeMovementGame from './game/StackGame.js'
+import "./style.css";
+import FreeMovementGame from "./game/StackGame.js";
 
 // DOM이 로드된 후 게임 초기화
-window.addEventListener('DOMContentLoaded', () => {
-    // 게임 시작
-    const game = new FreeMovementGame();
+window.addEventListener("DOMContentLoaded", () => {
+  // 게임 시작!
+  const game = new FreeMovementGame();
 });
